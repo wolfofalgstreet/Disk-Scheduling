d0278ebfd9bc418283b769254d87f81c50a84871
VegaIsaias <32isaias@gmail.com>
2018-07-30 13:41:14 -0400

    source

---
+++ DiskScheduler.go
@@ -0,0 +1,25 @@
+// Created by Isaias Perez Vega
+// Disk - Scheduling Algorithms
+// =================================================
+// Program reads an input file with cylinder requests
+// File specifies scheduling algorithm, program outputs
+// The optimal schedule based on algorithm.
+
+
+package main
+
+import {
+  "bufio"
+  "fmt"
+  //"strings"
+  //"os"
+}
+
+// --------------------------- //
+// Streamlining error checking //
+func checkErr(e error) {
+  if e != nil {
+    fmt.Println("Error ocurred when trying to open file.\n")
+    panic(e)
+  }
+}
